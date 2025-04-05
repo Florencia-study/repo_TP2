2ec412e4a61241c071dc9d6ac15428755447a6fd
Florencia <florenciaanabelintaschi@gmail.com>
2025-04-05 15:46:58 -0300

    agregados solo a rama 2

--- operadores_aritmeticos.py
+++ operadores_aritmeticos.py
@@ -6,3 +6,8 @@ print (9 % 5)
 print (9 * 5)
 print (9 ** 5)
 print (2 + 3 * 4)
+
+
+nombre = "Florencia"
+print (f"{nombre},  usted está practicando operaciones aritméticas en Python.")
+print ("Solo rama 2")
